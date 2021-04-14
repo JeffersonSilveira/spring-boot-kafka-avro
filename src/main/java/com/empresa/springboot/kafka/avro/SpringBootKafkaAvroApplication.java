17d1fa17e2fa46c3168b85950f2c97f20373acad
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2021-04-14 19:53:02 -0300

    fluxo de producer/consumer avrocom spring boot

---
+++ src/main/java/com/empresa/springboot/kafka/avro/SpringBootKafkaAvroApplication.java
@@ -0,0 +1,10 @@
+package com.empresa.springboot.kafka.avro;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaAvroApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaAvroApplication.class, args);
+	}
+}
